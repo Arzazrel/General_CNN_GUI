29916f76cb446e83c7577671cfe8d14c58368f2b
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2025-04-29 11:30:08 +0200

    Create check.py

---
+++ check.py
@@ -0,0 +1,6 @@
+import os
+import tensorflow as tf
+
+print("TensorFlow version:", tf.__version__)
+print("Built with CUDA:", tf.test.is_built_with_cuda())
+print("GPU Available:", tf.config.list_physical_devices('GPU'))
